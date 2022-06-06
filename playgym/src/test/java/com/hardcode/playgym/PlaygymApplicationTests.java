fb99fa6487061f8c2d56a776db8ca9498ba0e009
Bruno Cesar Caruso <brunoccaruso.s@gmail.com>
2022-06-06 20:42:19 -0300

    Primer commit

---
+++ playgym/src/test/java/com/hardcode/playgym/PlaygymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardcode.playgym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
